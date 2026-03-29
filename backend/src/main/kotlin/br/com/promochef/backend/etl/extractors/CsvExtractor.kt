bdd6649574f5162eefbbf7a72398e5353d5253ee
ironnia <mrironnia@gmail.com>
2026-03-29 13:11:38 -0300

    chore: Estruturação incial do Projeto PAW. SpringBoot, Kotlin, React e as pastas para Documentação, Configuração do Banco de dados.

---
+++ backend/src/main/kotlin/br/com/promochef/backend/etl/extractors/CsvExtractor.kt
@@ -0,0 +1,11 @@
+package br.com.promochef.backend.etl.extractors
+
+import org.springframework.stereotype.Component
+
+@Component
+class CsvExtractor {
+
+    fun testarExtracao() {
+        println("test")
+    }
+}
